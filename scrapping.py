55ab1120814c615b301beb30f705c5c4e2a853a6
SuicV <drissoulhbibcontact@gmail.com>
2022-03-10 22:05:10 +0100

    scrap reviews on Paris hotels

--- scrapping.py
+++ scrapping.py
@@ -1,5 +1,5 @@
 from Scrappers.TripAdvisorScrapper import TripAdvisorScrapper
 
-scrapper = TripAdvisorScrapper("data/reviews_paris_hotels.csv")
+scrapper = TripAdvisorScrapper("data/reviews_paris_hotels_1.csv")
 
 scrapper.start("Paris hotels")
