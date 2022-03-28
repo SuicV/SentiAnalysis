cc429b0616dea96d5723f6fa000ddc6b2e0a630d
SuicV <drissoulhbibcontact@gmail.com>
2022-03-28 00:56:31 +0000

    fix typos

--- UI/main.py
+++ UI/main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from utils.navbar import navbar
-from Preprocessors.ReviewPreprocessor import ReviewPreprocessor
 
-st.set_page_config(page_title='Topic Detection', layout="wide")
+st.set_page_config(page_title='Opinion mining', layout="wide")
 
 nv_bar = navbar()
