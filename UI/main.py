95836266fa822a108b2a93a03543dc14c6fc5896
SuicV <drissoulhbibcontact@gmail.com>
2022-03-30 12:05:04 +0000

    add all pages, fix import project modules using sys 📦

--- UI/main.py
+++ UI/main.py
@@ -1,11 +1,24 @@
-from matplotlib.pyplot import bar
+import sys
+if "E:\\PychCarmProjects\\s4\\ServQC_ML" not in sys.path:
+    sys.path.append("E:\\PychCarmProjects\\s4\\ServQC_ML")
+
 import streamlit as st
 from utils.navbar import navbar
-from pages import *
+# pages imports
+from pages.home import home
+from pages.preprocessing_page import preprocessing_page
+from pages.aspect_extraction_page import aspects_extraction_page
+from pages.sentiment_classification_page import sentiment_classification_page
 
 st.set_page_config(page_title='Opinion mining', layout="wide")
 
 nv_bar = navbar()
 
 if nv_bar == "Home":
-    home.home()
+    home()
+elif nv_bar == "Reviews Preprocessing":
+    preprocessing_page()
+elif nv_bar == "Aspects Extraction":
+    aspects_extraction_page()
+elif nv_bar == "Sentiments Classification":
+    sentiment_classification_page()
