e6689f56cc93d75151ee41114840e669bac341b0
SuicV <drissoulhbibcontact@gmail.com>
2022-04-16 22:26:56 +0000

    remove number of pages for agoda

--- UI/froms/scraping/scrap_by_link_form.py
+++ UI/froms/scraping/scrap_by_link_form.py
@@ -5,14 +5,12 @@ def scrap_by_link_form():
         col1, col2 = st.columns(2)
         with col1:
             tripadvisor_url = st.text_input("tripadvisor hotel url", value="")
-            tripadvisor_url_pages = st.number_input("number of pages to scrap from tripadvisor", min_value=1, max_value=100)
             pass
         with col2:
             agoda_url = st.text_input("agoda hotel url", value="")
-            agoda_url_pages = st.number_input("number of pages to scrap from agoda", min_value=1, max_value=100)
             pass
+        num_pages = st.number_input("Number of pages to scrap", min_value=1, max_value=100)
         link_form_submition_button = st.form_submit_button()
-        data = {"tripadvisor_url": tripadvisor_url, "tripadvisor_url_pages": tripadvisor_url_pages,
-            "agoda_url": agoda_url, "agoda_url_pages": agoda_url_pages}
+        data = {"tripadvisor_url": tripadvisor_url, "agoda_url": agoda_url, "num_pages": num_pages}
         return data, link_form_submition_button
     pass
