515a43fabe4e8b32ef7cc4142c1ae90df9c39b2f
SuicV <drissoulhbibcontact@gmail.com>
2022-04-16 00:34:27 +0000

    add scraping page

--- UI/main.py
+++ UI/main.py
@@ -9,13 +9,15 @@ from pages.home import home
 from pages.preprocessing_page import preprocessing_page
 from pages.aspect_extraction_page import aspects_extraction_page
 from pages.sentiment_classification_page import sentiment_classification_page
-
+from pages.scraping_page import scraping_page
 st.set_page_config(page_title='Opinion mining', layout="wide")
 
 nv_bar = navbar()
 
 if nv_bar == "Home":
     home()
+if nv_bar == "Reviews Scraping":
+    scraping_page()
 elif nv_bar == "Reviews Preprocessing":
     preprocessing_page()
 elif nv_bar == "Aspects Extraction":
