12954395963037eee35c672fedcc69fc18c4292c
SuicV <drissoulhbibcontact@gmail.com>
2022-05-04 16:06:27 +0000

    add word_sentiment that use sentiwordnet lexicon to get word polarity

---
+++ utils/word_sentiment.py
@@ -0,0 +1,28 @@
+from nltk.corpus import sentiwordnet as snw
+from statistics import mean
+
+
+def __get_wordnet_pos(tag):
+    if tag == "ADJ":
+        return "a"
+    elif tag == "NOUN":
+        return "n"
+    elif tag == "ADV":
+        return "r"
+    elif tag == "VERB":
+        return 'v'
+    return None
+
+def word_sentiment(word, POS, threshold = 0.2):
+    wn_pos = __get_wordnet_pos(POS)
+    if wn_pos :
+        synsets = snw.senti_synsets(word, wn_pos)
+        neg_scores = [synset.neg_score() for synset in synsets if synset.neg_score() > 0]
+        pos_scores = [synset.pos_socre() for synset in synsets if synset.pos_score() > 0]
+        neg_score = mean(neg_scores) if len(neg_scores) > 0  else 0
+        pos_score = mean(pos_scores) if len(pos_scores) > 0  else 0
+        if pos_score > neg_score and pos_score >= threshold:
+            return 1
+        elif neg_score > pos_score and neg_score >= threshold:
+            return -1
+    return 0
