976f88c26480fa90fc3801ed908e717a5c468d5c
SuicV <drissoulhbibcontact@gmail.com>
2022-06-17 11:42:04 +0100

    fix typos

--- UI/pages/home.py
+++ UI/pages/home.py
@@ -4,15 +4,14 @@ def home():
 
     st.write('--------------------------------------------')
     st.markdown("<h1 style='text-align: center'>Service Quality Controle</h1>", unsafe_allow_html=True)
-    st.markdown("<h5 style='text-align: center'>Nowadays, the abundance of online reviews about products and services, might be an indicator for enterprises about their performance and the satisfaction of their clients.<br /> For this reason, we created a system for sentiment analysis of online reviews, to analyze sentiment about different aspects of a business.</h5>", unsafe_allow_html=True)
+    st.markdown("<h5 style='text-align: center'>Nowadays, the abundance of online reviews about products and services, might be an indicator for business, especially hotels about their performance and the satisfaction of their clients.<br /> For this reason, we created a system for sentiment analysis of online reviews, to analyze sentiment about different aspects of an hotel.</h5>", unsafe_allow_html=True)
         
     st.write(' ')
 
     st.markdown("<h4>Fonctionnalities:</h4>", unsafe_allow_html=True)
     st.markdown("1- Reviews Scrapping")
     st.markdown("2- Reviews Preprocessing")
-    st.markdown("3- Aspects Extraction")
-    st.markdown("4- Sentiment Classification")
+    st.markdown("3- Sentiment Classification")
 
     st.markdown('')
     st.markdown("<h6 style='text-align: center'>Made By: OULAHBIB Idriss</h6>", unsafe_allow_html=True)
